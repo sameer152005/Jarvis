b9ca91a726225a5d24030d0051cf19ad376811b3
Sameer <kumar.sameer2005@gmail.com>
2024-09-21 14:14:43 +0530

    Jarvis

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import eel
+
+from engine.features import *
+from engine.command import *
+from engine.auth import recoganize
+
+def start():
+    
+    eel.init("www")
+
+    playAssistantSound()
+    @eel.expose
+    def init():
+        eel.hideLoader()
+        speak("Ready for Face Authentication")
+        flag = recoganize.AuthenticateFace()
+        if flag == 1:
+            eel.hideFaceAuth()
+            speak("Face Authentication Successfull")
+            eel.hideFaceAuthSuccess()
+            speak("Hello, Welcome Sam , How can I Help You")
+            eel.hideStart()
+            playAssistantSound()
+
+        else:
+            speak("Face Authentication Fail")
+
+    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+    eel.start('index.html', mode=None, host='localhost', block=True)
